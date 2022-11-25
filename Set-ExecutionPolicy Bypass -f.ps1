88a42afc8cde7b2cac7a551ffe45fbe04625050d
b11361213 <114211720+b11361213@users.noreply.github.com>
2022-11-25 11:39:39 +0800

    Updated Set-ExecutionPolicy Bypass -f.ps1 - 將滑鼠靈敏度從 4 改為 6

--- Set-ExecutionPolicy Bypass -f.ps1
+++ Set-ExecutionPolicy Bypass -f.ps1
@@ -1,6 +1,6 @@
 # Set-ExecutionPolicy Bypass -f.ps1
 
-reg add "HKCU\Control Panel\Mouse" /f /v MouseSensitivity /d 4
+reg add "HKCU\Control Panel\Mouse" /f /v MouseSensitivity /d 6
 
 $lang = New-WinUserLanguageList zh-TW
 $lang.Add("en-US")
