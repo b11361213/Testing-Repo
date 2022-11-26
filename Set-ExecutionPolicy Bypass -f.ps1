7fd27ed1a327f1a6becc7d90d644bcc45497a624
b11361213 <114211720+b11361213@users.noreply.github.com>
2022-11-26 02:41:47 +0000

    Updated Set-ExecutionPolicy Bypass -f.ps1 - 新增環境變數、滑鼠靈敏度修改

--- Set-ExecutionPolicy Bypass -f.ps1
+++ Set-ExecutionPolicy Bypass -f.ps1
@@ -1,10 +1,23 @@
 # Set-ExecutionPolicy Bypass -f.ps1
 
-reg add "HKCU\Control Panel\Mouse" /f /v MouseSensitivity /d 6
+$GCC_PATH = "C:\MinGW\bin"
+
+reg add "HKCU\Control Panel\Mouse" /f /v MouseSensitivity /d 5
+# 更改滑鼠靈敏度
 
 $lang = New-WinUserLanguageList zh-TW
 $lang.Add("en-US")
+$lang.Add("ja-JP")
 
 Set-WinUserLanguageList $lang
+# 設定語言
+
+$path = [System.Environment]::GetEnvironmentVariable("PATH", "user")
+$add = $GCC_PATH
+# user PATH: "user", system PATH: "machine"
+
+[System.Environment]::SetEnvironmentVariable("PATH", $path + ";" + $add, "user")
+# 新增 gcc 環境變數
 
 shutdown /l
+# 重新啟動使機碼變更生效
