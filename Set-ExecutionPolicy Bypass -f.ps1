8092a6b329e61e5cf730b0b88982b66789bc2887
b11361213 <114211720+b11361213@users.noreply.github.com>
2023-04-24 17:07:07 +0800

    Updated Set-ExecutionPolicy Bypass -f.ps1 - 調整指令順序

--- Set-ExecutionPolicy Bypass -f.ps1
+++ Set-ExecutionPolicy Bypass -f.ps1
@@ -3,8 +3,12 @@
 $GCC_PATH = "C:\MinGW\bin"
 $SQLITE_PATH = "C:\SQLite"
 
-reg add "HKCU\Control Panel\Mouse" /f /v MouseSensitivity /d 5
-# 更改滑鼠靈敏度
+$path = [System.Environment]::GetEnvironmentVariable("PATH", "user")
+# user PATH: "user", system PATH: "machine"
+
+[System.Environment]::SetEnvironmentVariable("PATH", $path + ";" + $SQLITE_PATH + ";" + $GCC_PATH, "user")
+# 新增 SQLite 環境變數與 GCC 環境變數
+
 
 $lang = New-WinUserLanguageList zh-TW
 $lang.Add("en-US")
@@ -13,11 +17,8 @@ $lang.Add("ja-JP")
 Set-WinUserLanguageList $lang -f
 # 設定語言
 
-$path = [System.Environment]::GetEnvironmentVariable("PATH", "user")
-# user PATH: "user", system PATH: "machine"
-
-[System.Environment]::SetEnvironmentVariable("PATH", $path + ";" + $SQLITE_PATH + ";" + $GCC_PATH, "user")
-# 新增 SQLite 環境變數與 GCC 環境變數
+reg add "HKCU\Control Panel\Mouse" /f /v MouseSensitivity /d 5
+# 更改滑鼠靈敏度
 
 winget install Google.Chrome
 # 更新 Google Chrome
